b54ec476091037aeaa22f1970c04b93fa5ad7d3e
Salman <ssohrab375@gmail.com>
2022-04-18 14:16:10 +0530

    optimised_my_code

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 
-
+/*
 displayArea = document.getElementById('display');
 function isOperator(buttonText) {
-    if (buttonText != 'DEL' || buttonText != 'CLR')
-
+    if (displayArea.innerText.length >= 24) {
+        return;
+    }
+    if (buttonText != 'DEL' || buttonText != 'CLR' || buttonText != '=') {
         if (buttonText == '-' || buttonText == '+' || buttonText == '*' || buttonText == '/' || buttonText == '%') {
             return true;
         }
         else {
             return false;
         }
+    }
 }
 
 function buttonClicked(buttonText) {
     if (buttonText != 'DEL' && buttonText != 'CLR' && buttonText != '=') {
 
         if (displayArea.innerText == '0') {
-            if (buttonText !='0' && buttonText != '00') {
+            if (buttonText != '0' && buttonText != '00') {
                 if (isOperator(buttonText) || buttonText == '.') {
                     displayArea.innerText = displayArea.innerText + buttonText;
                 }
@@ -29,8 +32,8 @@ function buttonClicked(buttonText) {
         }
         else {
             //leave
-            if (isOperator(displayArea.innerText[displayArea.innerText.length - 1]) && isOperator(buttonText) || (displayArea.innerText[displayArea.innerText.length - 1]=='.') && (buttonText =='.')) {
-                displayArea.innerText = displayArea.innerText.substring(0, displayArea.innerText.length - 1);
+            if (isOperator(displayArea.innerText[displayArea.innerText.length - 1]) && isOperator(buttonText) || (displayArea.innerText[displayArea.innerText.length - 1] == '.') && (buttonText == '.')) {
+                displayArea.innerText = displayArea.innerText.slice(0,- 1);
                 displayArea.innerText = displayArea.innerText + buttonText;
             }
             else {
@@ -45,16 +48,79 @@ function buttonClicked(buttonText) {
             displayArea.innerText = '0';
         }
         else if (buttonText == 'DEL') {
-            displayArea.innerText = displayArea.innerText.substring(0, displayArea.innerText.length - 1);
+            displayArea.innerText = displayArea.innerText.slice(0,- 1);
         }
         else if (buttonText === '=') {
             displayArea.innerText = eval(displayArea.innerText);
         }
     }
 
+}*/
+
+
+
+displayArea = document.getElementById('display');
+function isOperator(buttonText) {
+    if (displayArea.innerText.length >= 24) {
+        return;
+    }
+    if (buttonText != 'DEL' || buttonText != 'CLR' || buttonText != '=') {
+        if (buttonText == '-' || buttonText == '+' || buttonText == '*' || buttonText == '/' || buttonText == '%') {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+}
+
+function buttonClicked(buttonText) {
+    if (buttonText == 'CLR') {
+        displayArea.innerText = '0';
+        return;
+    }
+
+    if (buttonText == 'DEL') {
+        displayArea.innerText = displayArea.innerText.slice(0, -1);
+        return;
+    }
+
+    if (buttonText == '=') {
+        displayArea.innerText = eval(displayArea.innerText);
+        return;
+    }
+
+    if (displayArea.innerText === '0') {
+        if (buttonText == '0' || buttonText == '00') {
+            return;
+        }
+        if (buttonText == '.' || isOperator(buttonText)) {
+            displayArea.innerText = displayArea.innerText + buttonText;
+            return;
+        }
+        displayArea.innerText = buttonText;
+    }
+    else {
+        if (isOperator(displayArea.innerText[displayArea.innerText.length - 1]) && isOperator(buttonText) || (displayArea.innerText[displayArea.innerText.length - 1] == '.') && (buttonText == '.')) {
+            displayArea.innerText = displayArea.innerText.slice(0, - 1);
+            displayArea.innerText = displayArea.innerText + buttonText;
+            return;
+        }
+        else {
+            displayArea.innerText = displayArea.innerText + buttonText;
+        }
+
+    }
 }
 
 
+
+
+
+
+
+
+    
 
 /*
 displayArea= document.getElementById('display');
