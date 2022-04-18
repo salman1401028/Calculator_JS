cfe8f9129f4776b1608d4547aeac71ca300be156
Salman <ssohrab375@gmail.com>
2022-04-18 13:03:15 +0530

    adding_some_functions

--- index.js
+++ index.js
@@ -1,36 +1,60 @@
+
+
 displayArea = document.getElementById('display');
 function isOperator(buttonText) {
-    if (buttonText == '-' || buttonText == '+' || buttonText == '*' || buttonText == '/' || buttonText == '%') {
-        return true;
-    }
-    else {
-        return false;
-    }
+    if (buttonText != 'DEL' || buttonText != 'CLR')
+
+        if (buttonText == '-' || buttonText == '+' || buttonText == '*' || buttonText == '/' || buttonText == '%') {
+            return true;
+        }
+        else {
+            return false;
+        }
 }
 
 function buttonClicked(buttonText) {
-    if (displayArea.innerText == '0') {
-        if (buttonText != '00') {
-            if (displayArea.innerText == isOperator() || displayArea.innerText == '.') {
+    if (buttonText != 'DEL' && buttonText != 'CLR' && buttonText != '=') {
+
+        if (displayArea.innerText == '0') {
+            if (buttonText !='0' && buttonText != '00') {
+                if (isOperator(buttonText) || buttonText == '.') {
+                    displayArea.innerText = displayArea.innerText + buttonText;
+                }
+                else {
+                    displayArea.innerText = buttonText;
+                }
+
+            }
+
+        }
+        else {
+            //leave
+            if (isOperator(displayArea.innerText[displayArea.innerText.length - 1]) && isOperator(buttonText) || (displayArea.innerText[displayArea.innerText.length - 1]=='.') && (buttonText =='.')) {
+                displayArea.innerText = displayArea.innerText.substring(0, displayArea.innerText.length - 1);
                 displayArea.innerText = displayArea.innerText + buttonText;
             }
-            displayArea.innerText = buttonText;
+            else {
+                displayArea.innerText = displayArea.innerText + buttonText;
+            }
+
         }
 
     }
     else {
-        if(isOperator(displayArea.innerText[displayArea.innerText.length -1]) && isOperator(buttonText)) //This will check if there 
-        {                                                                               //are multiple operators entered by user,
-            displayArea.innerText= displayArea.innerText.substring(0,displayArea.innerText.length-1);      // if found then it will delete the previous one.
-            displayArea.innerText= displayArea.innerText + buttonText;
+        if (buttonText == 'CLR') {
+            displayArea.innerText = '0';
         }
-        else{
-            displayArea.innerText = displayArea.innerText + buttonText;
+        else if (buttonText == 'DEL') {
+            displayArea.innerText = displayArea.innerText.substring(0, displayArea.innerText.length - 1);
         }
-        
+        else if (buttonText === '=') {
+            displayArea.innerText = eval(displayArea.innerText);
+        }
     }
 
 }
+
+
 
 /*
 displayArea= document.getElementById('display');
